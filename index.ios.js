d71d4e0821aa1dfb01580cf906057b5d82542784
corasan <henrypl360@gmail.com>
2016-04-16 13:25:15 -0400

    fir commit

---
+++ index.ios.js
@@ -0,0 +1,47 @@
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  View,
+  StatusBar
+} from 'react-native';
+
+class todo_app extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <StatusBar barStyle='light-content' />
+                <Text style={styles.titleBar}>ToDo List</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5F3F6'
+  },
+  titleBar: {
+      paddingTop: 40,
+      paddingBottom: 10,
+      textAlign: 'center',
+      backgroundColor: '#42144E',
+      color: '#FFFFFF',
+      fontSize: 20,
+      marginBottom: 30,
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('todo_app', () => todo_app);
